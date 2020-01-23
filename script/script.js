1a5161def4fb719c95ee199a5fa24180e190c0ff
pavelKuklin <pavelkuklinweb@gmail.com>
2020-01-23 10:00:04 +0500

    сделал основное ДЗ

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let money = 150;
+let income = 'text';
+let addExpenses = 2000;
+let deposit = 300;
+let mission;
+let period;
+
+alert('JS11: Урок #1:');
+
+console.log('Введение в язык, подключение javascript');

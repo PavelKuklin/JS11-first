5014f3fc96b62f063a229eec92db826a03b134d5
pavelKuklin <pavelkuklinweb@gmail.com>
2020-01-23 11:36:10 +0500

    сделал комменты

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
-let money = 150;
-let income = 'text';
-let addExpenses = 2000;
-let deposit = 300;
+let money; //установил let, так как судя по названиям переменные могут в будущем изменяться. Const нельзя создавать пустым.
+let income;
+let addExpenses;
+let deposit;
 let mission;
 let period;
 
-alert('JS11: Урок #1:');
+alert('JS11: Урок #1:'); //текст может быть любым. 
 
 console.log('Введение в язык, подключение javascript');
